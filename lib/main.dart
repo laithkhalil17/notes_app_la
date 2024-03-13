74b49d8355d8ee56a5d9909b92cd7560b6ba190d
laithkhalil17 <laithkhalil2810@gmail.com>
2024-03-13 22:33:32 +0300

    add custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark,fontFamily:'Poppins' ),
-      home: NotesView(),
+      home: const NotesView(),
     );
   }
 }
